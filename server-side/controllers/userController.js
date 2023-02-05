4f20dd224d081529521a7fa93b62805b2180df2a
Alabere-coder <alabere4real247@gmail.com>
2023-02-05 18:49:58 +0100

    jwt

--- server-side/controllers/userController.js
+++ server-side/controllers/userController.js
@@ -68,7 +68,14 @@ const loginUser = asyncHandler(async (req, res) => {
 
 
 const getMe = asyncHandler(async (req, res) => {
-    res.json({ message: 'User data list' })
+    const { _id, name, email } = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
+    // res.json({ message: 'User data list' })
 })
 
 
